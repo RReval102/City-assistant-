5e365cd4408587f54d2093843cb391b37c1bf128
Reval Sadrislamov <Reval.Sadrislamov@urfu.me>
2025-07-26 13:39:05 +0500

    Update models.py

--- city_assistant_project/models.py
+++ city_assistant_project/models.py
@@ -26,7 +26,6 @@ class Profile(models.Model)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='dating_profile')
     bio = models.TextField(max_length=500, blank=True, verbose_name='О себе')
     birth_date = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
-    # Другие поля для анкеты знакомств...
-
+    
     def __str__(self)
         return fПрофиль {self.user.username}
